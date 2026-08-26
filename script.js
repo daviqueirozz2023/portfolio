b76e7906a1a0e1d382ae82b9c573755a858d2b9d
Davi Amaral Queiroz <daviqueirozzgithub@gmail.com>
2026-08-26 18:05:51 -0300

    Atualização de código
    mudando a estrutura para ficar mais leve.

--- script.js
+++ script.js
@@ -30,53 +30,7 @@ function closeOverlay4() {
     document.getElementById('overlay4').classList.remove('active');
 }
 
-function openOverlay5() {
-    document.getElementById('overlay5').classList.add('active');
-}
 
-function closeOverlay5() {
-    document.getElementById('overlay5').classList.remove('active');
-}
-
-function openOverlay6() {
-    document.getElementById('overlay6').classList.add('active');
-}
-
-function closeOverlay6() {
-    document.getElementById('overlay6').classList.remove('active');
-}
-
-function openOverlay7() {
-    document.getElementById('overlay7').classList.add('active');
-}
-
-function closeOverlay7() {
-    document.getElementById('overlay7').classList.remove('active');
-}
-
-function openOverlay8() {
-    document.getElementById('overlay8').classList.add('active');
-}
-
-function closeOverlay8() {
-    document.getElementById('overlay8').classList.remove('active');
-}
-
-function openOverlay9() {
-    document.getElementById('overlay9').classList.add('active');
-}
-
-function closeOverlay9() {
-    document.getElementById('overlay9').classList.remove('active');
-}
-
-function openOverlay11() {
-    document.getElementById('overlay11').classList.add('active');
-}
-
-function closeOverlay11() {
-    document.getElementById('overlay11').classList.remove('active');
-}
 
 function openOverlayById(overlayId) {
     const overlay = document.getElementById(overlayId);
